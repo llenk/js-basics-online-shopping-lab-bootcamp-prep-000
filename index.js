efe15e9467c2602c74ab2a7741556c9ae977b340
Ellen Keal <ellenkeal@gmail.com>
2017-08-19 02:53:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,21 @@ function setCart(c) {
 
 function addToCart(item) {
   cart[cart.length] = new Object({[item]:Math.floor(Math.random()*100)})
-  console.log("${name} has been added to your cart.")
+  console.log(`${name} has been added to your cart.`)
   return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  }
+  else if (cart.length === 1) {
+    var string += 'In your cart, you have';
+    for (var imet in cart[0]) {
+      string += `${imet} at ${cart[0][imet]}.`
+    }
+    console.log(string);
+  }
 }
 
 function total() {
