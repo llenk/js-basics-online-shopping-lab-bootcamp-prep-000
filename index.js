edd33ff01aed2d6494fbad6bd5017a790ee2c1d5
Ellen Keal <ellenkeal@gmail.com>
2017-08-19 03:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,5 +76,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (typeof cardNumber === 'undefined') {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  else {
+    val = "Your total cost is \$" + total() + ", which will be charged to the card " + cardNumber + ".";
+    cart = [];
+    return val;
+  }
 }
