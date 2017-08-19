580744ce174a134f5d5b97d772dfdf07e2a49657
Ellen Keal <ellenkeal@gmail.com>
2017-08-19 02:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function setCart(c) {
 
 function addToCart(item) {
   cart[cart.length] = new Object({[item]:Math.floor(Math.random()*100)})
-  console.log("" + name + " has been added to your cart.")
+  var string = "";
+  console.log("" + item + " has been added to your cart.")
   return cart;
 }
 
